db6b4bed1b67ecc03edd9388304157ec1ec8d6ba
Helldogify <mahipaul.tak@gmail.com>
2016-10-15 23:28:14 -0600

    getting silhouette of image
    currently able to get outline

---
+++ CMakeFiles/stocktho.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stocktho.dir/Untitled.cpp.o"
+  "stocktho.pdb"
+  "stocktho"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stocktho.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
